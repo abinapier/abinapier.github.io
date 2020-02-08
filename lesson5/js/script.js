236d5ba0aee161e439f38298553ecd466303e672
Abigail Napier <abinapier@Abigails-MacBook-Pro-2.local>
2020-02-07 17:54:59 -0700

    final preston

--- lesson5/js/script.js
+++ lesson5/js/script.js
@@ -40,6 +40,15 @@ function showArticle(){
 	for(var i =0; i<paragraphs.length; i++){
 		paragraphs[i].style.display = "block";
 	}
-	document.getElementById("preview").style.display="none";
+	document.getElementsByClassName("preview")[0].style.display="none";
 }
+
+function showBanner(){
+	let date = new Date();
+	let day = date.getDay();
+	if(day==5){
+		document.getElementById("banner").style.display="block";
+	}
+}
+
 
