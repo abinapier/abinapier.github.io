f1ed2544477b939621806284925e5cecef368828
abinapier <abi.napier@yahoo.com>
2020-04-07 16:54:57 -0600

    final

--- salmonRiver/js/menu.js
+++ salmonRiver/js/menu.js
@@ -2,3 +2,7 @@ function toggleMenu(){
 	document.getElementsByTagName('ul')[0].classList.toggle("hide");
 	document.getElementsByTagName('h1')[0].classList.toggle("hide");
 }
+
+function goto(page){
+	window.location.href = page;
+}
